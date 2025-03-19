49089495c25c91585a08426654a2724c7895341d
Ulissesgsilva1910 <rm94765@estudante.fieb.edu.br>
2025-03-19 08:16:35 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {useState} from "react"
+
+function App(){
+
+  const [nome,setNome] = useState('Ronaldo')
+
+  return(
+
+    <div>
+      <h3>pizzaria 2c</h3>
+      <input
+      className="nome"
+      onChange={(e)=>{setNome(e.target.value)}}
+      name="nome"
+      type="text" />
+      <button
+      onClick={()=>{alert(nome)}}
+      className="botao">
+      CLIQUE AQUI 
+      </button>
+
+
+    </div>
+  )
+
+
+}
+
+export default App 
